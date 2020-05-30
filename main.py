50969603587bfb70b98ad6a5c91c8b397ca94392
Aniebiet James <aniebietjames22@gmail.com>
2020-05-30 17:49:30 -0500

    Implement Roman numeral identification
    Chord symbol parameter in Chord class will eventually allow for
    lead sheet symbols in addition to its current Roman numeral analysis.
    I avoided parameters for inversion position, chord root, chord quality,
    etc. because that is already conveyed in the chord symbol.
    Chord quality is interpreted from scale object, consistent with
    Aldwell textbook.
    
    Scale object allows identification of chords based on roman numerals
    which permits specification of chord progressions without
    stating exact chords; this will be the fundamental way to represent chords.
    
    Lead sheet symbols will most likely be used for more advanced
    harmonies in which a Roman numeral designation does not exist or
    is impractical (e.g., chord extensions, mode mixture).
    
    Static ChordTypes most likely occur near beginnings and endings.
    Variable ChordTypes most likely occur in the middle.
    
    Pitch class removes the octave designation that notes have.
    When describing chords (e.g., for a progression), you often don't
    need to know the voicing.

--- main.py
+++ main.py
@@ -6,34 +6,93 @@ class Engraver:
 
 	# 4/4 time signature
 	measure_duration = 1
+	scale_obj = None
+	roman_numerals = ("I", "II", "III", "IV", "V", "VI", "VII")
 
+class Scale(Engraver):
+
+	note_letters = ("A", "B", "C", "D", "E", "F", "G")
+
+	def create_chord_pitches(self, chosen_roman_numeral, is_triad) -> List[Pitch]:
+
+		root_index = self.roman_numerals.index(chosen_roman_numeral)
+		chord_pitches = [self.scale_pitches[root_index]]
+
+		if is_triad:
+			chordal_items = 3
+		else:
+			chordal_items = 4
+		pitch_index = root_index
+		for _ in range(chordal_items - 1):
+			pitch_index += 2 
+			pitch_index %= 7
+			chord_pitches.append(self.scale_pitches[pitch_index])
+
+		return chord_pitches
 
 class Chord(Engraver):
 
+	triads_figured_bass = {"": 0, "5/3": 0, "6": 1, "6/3": 1, "6/4": 2}
+	sevenths_figured_bass = {
+		"7": 0, "7/5/3": 0, "6/5": 1, "6/5/3": 1, "4/3": 2, "6/4/3": 2, 
+		"6/4/2": 3, "4/2": 3, "2": 3,
+	}
+
 	def __init__(
-	  self, symbol: str, duration: Union[int, float, Fraction], 
-	  relative_offset: Union[int, float, Fraction], 
-	  parent_absolute_offset: Union[int, float, Fraction]) -> None:
+	  self, chord_symbol: str, duration: Union[int, float, Fraction], 
+	  relative_offset: Union[int, float, Fraction] = 0, 
+	  parent_absolute_offset: Union[int, float, Fraction] = 0) -> None:
 		self.relative_offset = relative_offset
 		self.absolute_offset = relative_offset + parent_absolute_offset
 		self.duration = duration
+		self.pitches = []
+
+		if "V" in chord_symbol or "I" in chord_symbol:
+			for num in range(3, 0, -1):
+				attempted_roman_numeral = chord_symbol[:num]
+				if attempted_roman_numeral in self.roman_numerals:
+					bass_figure = chord_symbol[num:]
+					break
+			else:
+				raise ValueError
+
+			if bass_figure in triads_figured_bass:
+				inversion_position = triads_figured_bass[bass_figure]
+				is_triad = True
+			elif bass_figure in sevenths_figured_bass:
+				inversion_position = sevenths_figured_bass[bass_figure]
+				is_triad = False
+			else:
+				raise ValueError
+
+			self.pitches = self.scale_obj.create_chord_pitches(
+				attempted_roman_numeral, is_triad
+			)
 
 
 class ChordType:
 
 	def __init__(
-	  self, chord_designator: str, 
+	  self, chord_designator: str, chord_conditional: str, 
 	  duration: Union[int, float, Fraction] = 0) -> None:
 		self.duration = duration
+		self.chord_designator = chord_designator
+		self.chord_conditional = chord_conditional  
 
 	def realize(self) -> Chord:
-		pass
+		if self.chord_designator == "SINGLE":
+			return Chord(self.chord_conditional, self.duration)
 
 
 class Note(Engraver):
 
 	def __init__(self, midi_pitch: int) -> None:
 		self.midi_pitch = midi_pitch
+		self.pitch_name = ""
+
+
+class Pitch:
+	pass
 
 
 class Measure(Engraver):
@@ -89,10 +148,10 @@ class MiniPeriod(Phrase):
 		super().__init__(relative_offset, parent_absolute_offset, num_measures)
 		self.progressions = (
 			Progression( # e.g., I II6 V I
-				ChordType("RAW_I", self.measure_duration), 
-				ChordType("POS_SUBDOM_-1", self.measure_duration),
-				ChordType("POS_DOM_-1", self.measure_duration), 
-				ChordType("RAW_I", self.measure_duration),
+				ChordType("SINGLE", "I", self.measure_duration), 
+				ChordType("MULTI", "SUBDOM_-1", self.measure_duration),
+				ChordType("MULTI", "DOM_-1", self.measure_duration), 
+				ChordType("SINGLE", "I", self.measure_duration),
 			),
 		)
 
