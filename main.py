e8012ae656b3eae31e6484bee950a4752398db99
Aniebiet James <aniebietjames22@gmail.com>
2020-05-29 20:38:49 -0500

    Add basic function parameters and classes
    - Create relative and absolute offset attributes to track individual
    items within the score. Relative offsets are probably good for
    artistic decisions. Absolute offset attributes are probably good for
    exporting audio.
    
    - Will focus on creating an audio and visual output by simplifying
    settings as an initial starting point. I don't want to concern
    myself with too many details when I haven't worked out the basic
    functionality. This means I will use simple chord progressions,
    simple time signatures, etc.
    
    - The ChordType class is useful in automatically generating unique
    progressions with a compact description of conditions and absolutes.
    
    - The Engraver class was chosen as a base class so that most
    musical objects can access basic properties of a score. I decided not
    to make constants because I anticipate that settings like
    key signature and time signature may change as the piece progresses.
    
    - While it would seem that a Score object should house these types of
    properties, I implemented the Score object as a structural unit that
    builds large sections of a piece. Therefore, it is an unsuitable
    parent for classes such as Measure or Chord, given that the inherited
    methods would be nonsensical.

--- main.py
+++ main.py
@@ -1,57 +1,124 @@
 import random
+from typing import List, Tuple, Union
+from fractions import Fraction
 
-class Note:
+class Engraver:
+
+	# 4/4 time signature
+	measure_duration = 1
+
+
+class Chord(Engraver):
+
+	def __init__(
+	  self, symbol: str, duration: Union[int, float, Fraction], 
+	  relative_offset: Union[int, float, Fraction], 
+	  parent_absolute_offset: Union[int, float, Fraction]) -> None:
+		self.relative_offset = relative_offset
+		self.absolute_offset = relative_offset + parent_absolute_offset
+		self.duration = duration
+
+
+class ChordType:
+
+	def __init__(
+	  self, chord_designator: str, 
+	  duration: Union[int, float, Fraction] = 0) -> None:
+		self.duration = duration
+
+	def realize(self) -> Chord:
+		pass
+
+
+class Note(Engraver):
 
 	def __init__(self, midi_pitch: int) -> None:
 		self.midi_pitch = midi_pitch
 
 
-class Measure:
+class Measure(Engraver):
 
-	def __init__(self):
+	def __init__(
+	  self, relative_offset: Union[int, float, Fraction], 
+	  parent_absolute_offset: Union[int, float, Fraction]) -> None:
 		self.notes = []
+		self.chords = []
+		self.relative_offset = relative_offset
+		self.absolute_offset = relative_offset + parent_absolute_offset
 
-	def imprint_chord(self) -> None:
-		pass
+	def imprint_chord(self, chord: Chord) -> None:
+		if self.chords:
+			current_offset = self.chords[-1].relative_offset + self.chords[-1].duration
+		else:
+			current_offset = 0
+		self.chords.append(
+			Chord(chord.symbol, chord.duration, current_offset, self.absolute_offset)
+		)
 
 
-class Phrase:
+class Phrase(Engraver):
 
-	def __init__(self, num_measures: int = 8) -> None:
+	def __init__(
+	  self, relative_offset: Union[int, float, Fraction],
+	  parent_absolute_offset: Union[int, float, Fraction], 
+	  num_measures: int = 8) -> None:
 		self.measures = []
-		for _ in range(num_measures):
-			self.measures.append(Measure())
-
-	def identify_measure(self, time_marker: int) -> int:
-		pass
+		self.relative_offset = relative_offset
+		self.absolute_offset = relative_offset + parent_absolute_offset
+		for index in range(num_measures):
+			self.measures.append(
+				Measure(index * self.measure_duration, self.absolute_offset)
+			)
 
 	def imprint_progression(self) -> None:
-		progression_pattern = random.choice(self.progression_patterns)
-		chosen_progression = progression_pattern.realize()
-		time_marker = 0
+		progression = random.choice(self.progressions)
+		chosen_progression = progression.realize()
+		relative_offset = 0
 		for current_chord in chosen_progression:
-			measure_index = self.identify_measure(time_marker)
+			measure_index = relative_offset // self.measure_duration
 			self.measures[measure_index].imprint_chord(current_chord)
-			time_marker += current_chord.duration 
+			relative_offset += current_chord.duration 
 
 
 class MiniPeriod(Phrase): 
 
-	def __init__(self, num_measures: int = 4) -> None:
-		super().__init__(num_measures)
-		self.progression_patterns = [] 
+	def __init__(
+	  self, relative_offset: Union[int, float, Fraction],
+	  parent_absolute_offset: Union[int, float, Fraction], 
+	  num_measures: int = 4) -> None:
+		super().__init__(relative_offset, parent_absolute_offset, num_measures)
+		self.progressions = (
+			Progression( # e.g., I II6 V I
+				ChordType("RAW_I", self.measure_duration), 
+				ChordType("POS_SUBDOM_-1", self.measure_duration),
+				ChordType("POS_DOM_-1", self.measure_duration), 
+				ChordType("RAW_I", self.measure_duration),
+			),
+		)
 
 	def add_melody_notes(self) -> None:
 		pass
 
 
-class Score:
+class Progression:
+	
+	def __init__(self, *args: Tuple[ChordType, ...]) -> None:
+		self.chord_types = args
 
-	def __init__(self):
+	def realize(self) -> List[Chord]:
+		chord_sequence = []
+		for chord_type in self.chord_types:
+			chord_sequence.append(chord_type.realize())
+		return chord_sequence
+
+
+class Score(Engraver):
+
+	def __init__(self) -> None:
 		self.phrases = []
 
 	def create_theme(self) -> None:
-		new_phrase = MiniPeriod()
+		new_phrase = MiniPeriod(0, 0)
 		new_phrase.imprint_progression()
 		new_phrase.add_melody_notes()
 		self.phrases.append(new_phrase)
